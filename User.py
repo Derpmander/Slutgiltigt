bd3116e9305c1023cca10e16b81e3560c58465a7
Derpmander <70146049+Derpmander@users.noreply.github.com>
2021-04-19 14:19:37 +0200

    n

--- User.py
+++ User.py
@@ -1,14 +1,19 @@
-#def oPen ():
-    #file = input("Please enter the name of the file: ")
-    #with open (file, "r", encodning="utf-8") as list:
-    #    return list
-#def listDef (Förnamn, Efternamn):
-    #if len(list): 
+def oPen ():
+    file = input("Please enter the name of the file: ")
+    with open (file, "r", encodning="utf-8") as list:
+        return list
+
+def username (Förnamn, Efternamn):
+    rows = list.readlines()
+    for row in rows:
+        splitedRow = row.split(",")
+        username = splitedRow[1][1] + splitedrow[1] 
+
 
 file = input("Please enter the name of the file: ")
 with open (file, "r") as list:
     rows = list.readlines()
     for row in rows:
-        splitedRow = row.split()
-        username = splitedRow[1] + splitedRow[2] + splitedRow[4]
+        splitedRow = row.split(",")
+        username = splitedRow[1][1] + splitedRow[2] + splitedRow[4] 
     print (username)  
